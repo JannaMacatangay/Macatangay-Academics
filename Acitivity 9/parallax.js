ade0f8cd1530750df78b75dad99e3614764bd7af
jannerch <jannamaemacatangay5@gmail.com>
2024-11-18 23:54:07 +0800

    parallax

--- Acitivity 9/parallax.js
+++ Acitivity 9/parallax.js
@@ -11,7 +11,7 @@ function castParallax() {
 		for (var i = 0; i < layers.length; i++) {
 			layer = layers[i];
 			speed = layer.getAttribute('data-speed');
-			var yPos = -(top * speed / 100);
+			var yPos = -(top * speed / 60);
 			layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
 		}
 	});
