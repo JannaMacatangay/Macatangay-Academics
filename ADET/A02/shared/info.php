84b8bc452d442bf6013859ab4da971ef25ad035d
jannerch <jannamaemacatangay5@gmail.com>
2025-04-12 13:33:36 +0800

    minimal changes

--- ADET/A02/shared/info.php
+++ ADET/A02/shared/info.php
@@ -4,22 +4,22 @@
     <div class="col-md-8">
     <ul class="list-unstyled d-flex justify-content-between flex-wrap">
         <li>
-            <a class="navbar-brand" href="https://www.facebook.com/jannerchh">
+            <a class="navbar-brand" href="https://www.facebook.com/jannerchh" target="_blank">
               <img src="img/icon/fb.png" alt="Logo" style="width: auto; height: 2rem; gap: 1rem;">
              </a>
         </li>
         <li>
-            <a class="navbar-brand" href="https://www.instagram.com/jannerch_/">
+            <a class="navbar-brand" href="https://www.instagram.com/jannerch_/" target="_blank">
               <img src="img/icon/ig.png" alt="Logo" style="width: auto; height: 2rem; gap: 1rem;">
              </a>
         </li>
         <li>
-            <a class="navbar-brand" href="jannamacatangay.github.io">
+            <a class="navbar-brand" href="https://github.com/JannaMacatangay" target="_blank">
               <img src="img/icon/gh.png" alt="Logo" style="width: auto; height: 2rem; gap: 1rem;">
              </a>
         </li>
         <li>
-            <a class="navbar-brand" href="https://www.linkedin.com/in/janna-macatangay-19b885295/">
+            <a class="navbar-brand" href="https://www.linkedin.com/in/janna-macatangay-19b885295/" target="_blank">
               <img src="img/icon/li.png" alt="Logo" style="width: auto; height: 2rem; gap: 1rem;">
              </a>
         </li>
